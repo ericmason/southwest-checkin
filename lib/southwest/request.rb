8f4c7c6d4cba6185efd1e4d53041ef673a9e3604
David Shepherd <1191800+dtshepherd@users.noreply.github.com>
2017-11-26 15:01:40 -0500

    re-integrate with Southwest API changes

--- lib/southwest/request.rb
+++ lib/southwest/request.rb
@@ -20,15 +20,15 @@ module Southwest
     end
 
     def base_uri
-      'https://api-extensions.southwest.com/v1/mobile'
+      'https://mobile.southwest.com/api/extensions/v1/mobile'
     end
 
     def user_agent
-      "Southwest/3.3.7 (iPhone; iOS 9.3; Scale/2.00)"
+      "Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4"
     end
 
     def api_key
-      "l7xx8d8bfce4ee874269bedc02832674129b"
+      "l7xx12ebcbc825eb480faa276e7f192d98d1"
     end
   end
 end
